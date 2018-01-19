34e7ca7ed6d0822950e14904f4e4923267c6aa7d
Adibas03 <adibas03@ymail.com>
2018-01-19 11:21:13 +0100

    First bank draft

---
+++ contracts/BusFundBank.sol
@@ -0,0 +1,64 @@
+import './WithUpgradeableInterface.sol';
+import './SafeMath.sol';
+
+pragma solidity^0.4.18;
+
+contract BusFundBank is TimedOwnable,WithDevilUpgradeableInterface{
+  using SafeMath for uint;
+
+  uint256 feesBalance;
+
+  event EtherTransfer(address to,uint256 amount);
+
+  function BusFundBank(address _interface) UpgradeableInterface(_interface) public {
+
+  }
+
+  function getTokenBalance( address _token) public pure constant returns(uint256)
+    return ERC20(_token).balanceOf(this);
+  }
+
+  function sendTokens(address _token,  address _to,uint256 _value) public onlyInterface isInterfaceSet {
+    require( getTokenBalance(_token) >= _value );
+    ERC20(_token).transfer(_to,_value);
+  }
+
+  function sendBatchTokens(address _token, address[20] _addresses, uint256[20] _values ) public onlyInterface isInterfaceSet {
+    for(uint256  t=0; t<_addresses.length; t++){
+      if(_addresses[t] !== 0x0 && _values[t] > 0)
+        sendTokens(_token,_addresses[t],_values[t]);
+    }
+  }
+
+  function sendEther(address _to,uint256 _value) public onlyInterface isInterfaceSet {
+    require(_to !== 0x0);
+    require(_value > 0);
+    EtherTransfer(_to,_value);
+    _to.transfer(_value);
+  }
+
+  function withdrawFees() public onlyInterface isInterfaceSet {
+    require(this.balance >= feesBalance);
+    uint256 toPay = feesBalance;
+    feesBalance = 0;
+    EtherTransfer(owner,toPay);
+    owner.transfer(toPay);
+  }
+
+  function confirmSetInterface() payable public onlyOwner {
+    super.confirmSetInterface();
+    feesBalance.add(changeInterfaceCost);
+  }
+
+  function rejectSetInterface() payable public onlyOwner {
+    super.rejectSetInterface();
+    feesBalance.add(msg.value.sub(changeInterfaceCost) );
+  }
+
+  function (uint256 fees) public payable{
+    require(msg.value >= fees);
+    feesBalance.add(fees);
+  }
+
+  function () public payable{}
+}
