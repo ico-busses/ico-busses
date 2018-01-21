3460e803891a82d74c4f82382f9fef20f3cf4fee
Adibas03 <adibas03@ymail.com>
2018-01-21 16:07:51 +0100

    update TimedOwnable

--- contracts/TimedOwnable.sol
+++ contracts/TimedOwnable.sol
@@ -1,4 +1,4 @@
-
+import '../base_contracts/Ownable.sol';
 
 pragma solidity^0.4.18;
 
@@ -6,15 +6,16 @@ contract TimedOwnable is Ownable{
 
   uint256 transferInitiated = 0;
   uint256 transferWaitTime = 30 minutes;
-  event transferOwnershipRequested( address newOwner, uint256 block.timestamp);
+  event transferOwnershipRequested( address newOwner, uint256 timestamp);
 
-  function transferOwnership(address newOwner) onlyOwner public
+  function transferOwnership(address newOwner) onlyOwner public{
     transferInitiated = block.timestamp;
     super.transferOwnership(newOwner);
   }
 
   function acceptOwnership() {
     require( (now - transferInitiated) > transferWaitTime);
+    transferInitiated = 0;
     super.acceptOwnership();
   }
 }
