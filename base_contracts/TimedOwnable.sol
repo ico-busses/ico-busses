9da7349322356141f192a6370708e4e7bf45ee34
Adibas03 <adibas03@ymail.com>
2018-01-21 18:39:51 +0100

    Add RejectOwnership to `TimedOwnable`

--- base_contracts/TimedOwnable.sol
+++ base_contracts/TimedOwnable.sol
@@ -8,12 +8,17 @@ contract TimedOwnable is Ownable{
   uint256 public transferOwnerWaitTime = 30 minutes;
   event transferOwnershipRequested( address newOwner, uint256 timestamp);
 
-  function transferOwnership(address newOwner) onlyOwner public{
+  function transferOwnership(address newOwner) onlyOwner public {
     transferOwnerInitiated = block.timestamp;
     super.transferOwnership(newOwner);
   }
 
-  function acceptOwnership() {
+  function rejectTransferOwnership() onlyOwner public {
+    transferOwnerInitiated = 0;
+    newOwner = 0;
+  }
+
+  function acceptOwnership() onlyOwner public {
     require( (now - transferOwnerInitiated) > transferOwnerWaitTime);
     transferOwnerInitiated = 0;
     super.acceptOwnership();
