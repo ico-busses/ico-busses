28e30eb74fb7e382e2dd16a8221308799fd95950
Adibas03 <adibas03@ymail.com>
2018-01-28 15:59:02 +0100

    Fix TImedOwnable, Introduce Reconciler( coFounder)

--- base_contracts/TimedOwnable.sol
+++ base_contracts/TimedOwnable.sol
@@ -4,24 +4,39 @@ pragma solidity^0.4.18;
 
 contract TimedOwnable is Ownable{
 
+  address public coFounder;
   uint256 public transferOwnerInitiated = 0;
   uint256 public transferOwnerWaitTime = 30 minutes;
   event transferOwnershipRequested( address newOwner, uint256 timestamp);
 
+  function TimedOwnable(address _coFounder) Ownable(){
+    require(_coFounder != 0x0);
+    coFounder = _coFounder;
+  }
+
   function transferOwnership(address newOwner) onlyOwner public {
     require(transferOwnerInitiated == 0);
     transferOwnerInitiated = block.timestamp;
+    transferOwnershipRequested( newOwner, transferOwnerInitiated);
     super.transferOwnership(newOwner);
   }
 
   function rejectTransferOwnership() onlyOwner public {
     transferOwnerInitiated = 0;
-    newOwner = 0;
   }
 
-  function acceptOwnership() onlyOwner public {
+  function acceptOwnership() public {
+    require(transferOwnerInitiated > 0);
     require( (now - transferOwnerInitiated) >= transferOwnerWaitTime);
     transferOwnerInitiated = 0;
     super.acceptOwnership();
+  }
+
+  function vetoTransferOwnership() public {
+    require(coFounder == msg.sender);
+    require(transferOwnerInitiated > 0);
+    transferOwnerInitiated = 0;
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 }
