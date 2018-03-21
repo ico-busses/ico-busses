2762f5d4c5ce494d395e967909c132f12020c7a5
Adibas03 <adibas03@ymail.com>
2018-03-22 00:11:51 +0100

    update FundBank tests

--- test/03_BusFundBank.js
+++ test/03_BusFundBank.js
@@ -1,4 +1,5 @@
 var BusFundBank = artifacts.require('./BusFundBank.sol');// Import contract of StandarTOken type
+var DummyToken = artifacts.require('./DummyToken.sol');// Import contract of StandarTOken type
 
 contract('03_BusFundBank', function(accounts){
 
@@ -15,6 +16,14 @@ contract('03_BusFundBank', function(accounts){
     newBusFundBank = function(){
       return BusFundBank.new(
           deployment_config._interface,
+          {from:Me}
+      );
+    };
+
+    newToken = function(){
+      return DummyToken.new(
+          'Dummy',
+          'DMY',
           {from:Me}
       );
     };
@@ -45,7 +54,7 @@ contract('03_BusFundBank', function(accounts){
     });
     describe('EtherTransfer',function(){
 
-        it('Should fail to send funds to the FundBank',function(done){
+        it('Should fail to send funds to the FundBank fallback',function(done){
             var _value = web3.toWei(1, 'ether');
             web3.eth.sendTransaction({from:Me,to:contract.address,value:_value},function(e,r){
               assert.notEqual(e,null,`Illegally sent ${_value/1e+18} Eth to FundBank`);
@@ -85,119 +94,129 @@ contract('03_BusFundBank', function(accounts){
             done();
           });
         });
+    })
 
-        it.skip('Should send right amount to the contract from lender',function(done){
-          var _lender = contract.lender.call(),
-          _value = contract.getLoanValue.call(true),
-          _mybal = web3.eth.getBalance(Me),
-          txn = {from:_lender,to:contract.address,value: _value, gas: 210000 };
+    describe('TokenTransfer',function(){
+        var token;
+        it("Should create dummy token", function (done){
+          newToken()
+          .then(function(inst){
+              token = inst.contract;
+              web3 = inst.constructor.web3;
 
-          web3.eth.sendTransaction(txn,function(e,r){
-            var balance = contract.balanceOf.call(_lender),
-            totalSupply = contract.actualTotalSupply.call();
-            _mynewbal = web3.eth.getBalance(Me);
-            assert.equal(e,null,'Loan not successfully funded by lender');
-            assert.equal(Number(balance),Number(totalSupply),'Wrong number of tokens assigned to lender');
-            assert.equal(Number(_mynewbal),Number(_mybal)+ deployment_config._initialAmount,'Wrong value of Ether sent to Owner');
-            done();
+              console.log('Token Address:',token.address );
+
+              console.log('Owner:', token.owner.call());
+
+              assert.notEqual(token.address, null, 'Contract not successfully deployed');
+              done();
           });
+        })
+        it('Should access FundBank token balance',function(){
+          var _value = web3.toWei(1,'ether');
+          var _balance = contract.getTokenBalance.call(token.address);
+          assert.notEqual(_balance,null, 'Unable to fetch FundBank\'s balance');
+
+          token.mint( contract.address, _value, {from:Me}, function(e,r) {
+            assert.equal(e,null, 'Unable to mint new tokens to contract');
+            var _newbalance = contract.getTokenBalance.call(token.address);
+            var _diff = _newbalance.minus(_balance);
+            assert.equal(_diff.toNumber(),_value, `${_diff.toNumber()} was minted instead of ${_value}`);
+          })
+        })
+
+        it('Rogue Address Should fail to send tokens from FundBank',function(done){
+          var _balance = contract.getTokenBalance.call(token.address);
+          assert.notEqual(_balance,null, 'Unable to fetch FundBank\'s token balance');
+          var _toSend = _balance.dividedBy(5);
+
+          contract.sendTokens( token.address, accounts[4], _toSend, {from:accounts[3]}, function(e,r) {
+            assert.notEqual(e,null, 'Sent tokens fron FundBank without permission');
+            done();
+          })
+        })
+
+        it('Interface Should send tokens from FundBank',function(done){
+          var _balance = token.balanceOf.call(accounts[4]);
+          var _busbalance = contract.getTokenBalance.call(token.address);
+          assert.notEqual(_balance,null, 'Unable to account balance');
+          assert.notEqual(_busbalance,null, 'Unable to fetch FundBank\'s token balance');
+          var _toSend = _busbalance.dividedBy(5);
+
+          contract.sendTokens( token.address, accounts[4], _toSend, {from:busData}, function(e,r) {
+            assert.equal(e,null, 'Interface failed to send tokens from FundBank');
+            var _newbalance = token.balanceOf.call(accounts[4]);
+            assert.equal(_newbalance.minus(_balance).toNumber(),_toSend.toNumber(), `${_newbalance.minus(_balance).toNumber()} tokens sent instead of ${_toSend.toNumber()}`);
+            done();
+          })
+        })
+
+        it('Rogue Address Should fail to send batch tokens from FundBank',function(done){
+          var _balance = contract.getTokenBalance.call(token.address);
+          assert.notEqual(_balance,null, 'Unable to fetch FundBank\'s token balance');
+          var _toSend = _balance.dividedBy(5);
+          var addressList = [accounts[2],accounts[3],accounts[5]];
+          var _balances = [];
+          addressList.map( function(address,index){
+            _balances[index] = token.balanceOf.call(address);
+          })
+          var _toSends = addressList.map(function(){
+            return _toSend;
+          })
+
+          contract.sendBatchTokens( token.address, addressList, _toSends, {from:accounts[3]}, function(e,r) {
+            assert.notEqual(e,null, 'Sent tokens fron FundBank without permission');
+            done();
+          })
         });
-    })
 
-    describe.skip('Interest Accruing ',function(){
-        it('Should fetch isInterestStatusUpdated status',function(){
-            var isInterestStatusUpdated = contract.isInterestStatusUpdated.call();
-
-            assert.notEqual(isInterestStatusUpdated,null, 'Did not successfully fetch isInterestStatusUpdated value, instead "'+isInterestStatusUpdated+'"');
-        })
-
-        it('Should run updateInterest function from any address',function(done){
-
-          function doUpdate(){
-            contract.updateInterest({from:accounts[3]},function(e,r){
-              assert.equal(e,null,'Random address could not run updateInterest functon');
-              done()
-            });
-          }
-
-          //Update EVM time to required time
-          var time = deployment_config._loanTerm*deployment_config._dayLength*1000;
-          forceMine(time);
-
-          assert.equal(contract.isTermOver.call(),true,'Loan tern has not over ( '+web3.eth.getBlock('latest').timestamp+' )');
-          doUpdate();
-        })
-
-        it('Should not allow race condition on updateInterest function',function(done){
-          //Update EVM time to required time
-          var alldone=0,
-          time = deployment_config._loanCycle*2*deployment_config._dayLength*1000;
-          forceMine(time);
-
-          var actualTotalSupply = contract.actualTotalSupply.call();
-
-          contract.updateInterest({from:accounts[3]},function(e,r){
-            var totalSupply = contract.totalSupply.call();
-            assert.equal(Number(totalSupply) == Number(actualTotalSupply),true,'Leakage allowed mutiple runs of updateInterest: '+Number(totalSupply)+' !== '+Number(actualTotalSupply) );
-            alldone++;
-            checkDone();
+        it('Interface Should successfully send batch tokens from FundBank',function(done){
+          var _balance = contract.getTokenBalance.call(token.address);
+          assert.notEqual(_balance,null, 'Unable to fetch FundBank\'s token balance');
+          var _toSend = _balance.dividedBy(5);
+          var addressList = [accounts[2],accounts[3],accounts[5]];
+          var _balances = [];
+          addressList.map( function(address,index){
+            _balances[index] = token.balanceOf.call(address);
+          })
+          var _toSends = addressList.map(function(){
+            return _toSend;
           })
 
-          contract.updateInterest({from:accounts[3]},function(e,r){
-            var totalSupply = contract.totalSupply.call();
-            assert.equal(Number(totalSupply) == Number(actualTotalSupply),true,'Leakage allowed mutiple runs of updateInterest: '+Number(totalSupply)+' !== '+Number(actualTotalSupply) );
-            alldone++;
-            checkDone();
-          })
-
-          function checkDone(){
-            if(alldone>1)
-              done();
-          }
-
-        })
-
-        it('Should fail to allow owner run finishMinting function',function(done){
-          assert.isNotOk(contract.finishMinting, "finishMiting shall be available internally only");
-          done();
-        })
-
-    })
-
-    describe.skip('Loan Refund',function(){
-        it('Should fail to refund amount diffferent from total due',function(done){
-            var _value = contract.getLoanValue.call(true);//fetch the initial loan value
-            web3.eth.sendTransaction({from:Me,to:contract.address,value:_value},function(e,r){
-              assert.notEqual(e,null,'Owner refunded Loan with wrong (initial without interest) amount');
-              done();
-            });
-        })
-
-        it('Should fail to refund correct amount from non-owner',function(done){
-          var _value = contract.getLoanValue.call(false);//fetch the initial loan value
-          web3.eth.sendTransaction({from:accounts[3],to:contract.address,value:_value},function(e,r){
-            assert.notEqual(e,null,'Non-Owner successfully refunded Loan');
+          contract.sendBatchTokens( token.address, [20], [20], {from:busData}, function(e,r) {
+            assert.equal(e,null, 'Interface failed to send tokens from FundBank');
+            addressList.map( function(address,index){
+              _newbalance = token.balanceOf.call(address);
+              assert(_newbalance.minus(_balances[index]), _toSends[index], `${_newbalance.minus(_balances[index])} tokens sent instead of ${_toSends[index]}`);
+            })
             done();
+          })
+        })
+    })
+
+    describe('Fees handling',function(){
+
+        it('Should successfully check fees balance in FundBank',function(){
+          var feesBalance = contract.feesBalance.call();
+          assert.notEqual(feesBalance,null,'Fees balance could not be successfully fetched.');
+        })
+
+        it('Should fail to withdraw fees balance by non-owner',function(done){
+          var accountBalance = web3.eth.getBalance(accounts[5]);
+          var balancetoSend = accountBalance.div(2);
+          assert.equal(balancetoSend.gt(0),true,'Fee to be paid is not grater than 0');
+
+          contract.fund( balancetoSend, {from:accounts[5], value: balancetoSend}, function(e,r) {
+            assert.equal(e,null,'FundBank not successfully funded with fees');
+            var feesBalance = contract.feesBalance.call();
+            contract.withdrawFees( accounts[5], {from:coFounder}, function(e,r) {
+              assert.notEqual(e,null,'Non-owner successfully withdrew Fees');
+              done();
+            });
           });
         })
 
-        it('Should successfully refund correct amount',function(done){
-          var _value = contract.getLoanValue.call(false),//fetch the initial loan value
-          _lender = contract.lender.call(),
-          _lenderBalance = web3.eth.getBalance(_lender);
-
-          web3.eth.sendTransaction({from:Me,to:contract.address,value:_value},function(e,r){
-            var balance = contract.balanceOf.call(Me),
-            totalSupply = contract.actualTotalSupply.call();
-            _debtownernewbal = web3.eth.getBalance(_lender);
-            assert.equal(e,null,'Loan not successfully refunded by Owner');
-            assert.equal(Number(balance),Number(totalSupply),'Wrong number of tokens refunded to Owner');
-            assert.equal(Number(_debtownernewbal),Number(_lenderBalance)+ Number(_value),'Wrong value of Ether sent to lender');
-            done();
-          });
-        })
-
-        it('Should successfully refund before contract maturation',function(done){
+        it.skip('Should successfully refund before contract maturation',function(done){
           deployNewDebtContract()
           .then(function(inst){
               newcontract = inst.contract;
@@ -238,7 +257,7 @@ contract('03_BusFundBank', function(accounts){
           });
         });
 
-        it('Should confirm loanValue does not increase after refundLoan',function(done){
+        it.skip('Should confirm loanValue does not increase after refundLoan',function(done){
           var time = deployment_config._loanCycle*2*deployment_config._dayLength*1000;
           forceMine(time);
 
